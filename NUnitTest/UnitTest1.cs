8f7e1c820b7c0e1f093d2b69a31322ae938472bf
Neal Moran <nealmoran23@gmail.com>
2021-10-17 15:12:19 +0100

    Add project files.

---
+++ NUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void StringSetTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TDD_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// This is am implementation of the 
+// https://sites.google.com/site/tddproblems/all-problems-1/String-set
+// TDD exercise
+
+namespace TDD_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
